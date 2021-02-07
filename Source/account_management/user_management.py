cf4eb8af20a26ac5322dd9876a145170aac59c8e
pb97 <pavel.belitskiy@gmail.com>
2021-02-07 13:24:33 +0000

    Adds repeat email field and validation messages
    Forces password field to be equal to repeat password field.
    Adds repeat password field as flask form (bootstrap)
    Adds responsive messages for email and password

--- Source/account_management/user_management.py
+++ Source/account_management/user_management.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms.validators import InputRequired, Email, Length, EqualTo
 
 
 # Represent the Flask login form - will be passed into flask bootstrap at routing
@@ -17,14 +17,20 @@ class LoginForm(FlaskForm):
 # Represent the Flask user registration form - will be passed into flask bootstrap at routing
 class RegistrationForm(FlaskForm):
     email_message = "Please enter a valid email address"
-    username = StringField('Username', validators=[InputRequired(),
+    password_length_message = "Password must be between 8-80 characters"
+    password_confirm_message = "Passwords do not match"
+    username = StringField('Username - up to 10 characters', validators=[InputRequired(),
                                                    Length(min=4, max=10),
                                                    ])
     email = StringField('Email', validators=[InputRequired(),
                                              Email(message=str(email_message)),
-                                             Length(max=50)
+                                             Length(min=4, max=50, message=email_message)
                                              ])
     password = PasswordField('Password', validators=[InputRequired(),
-                                                     Length(min=8, max=80)
+                                                     Length(min=8, max=80,
+                                                            message=password_length_message),
+                                                     EqualTo('confirm_password',
+                                                             message=password_message)
                                                      ])
+    confirm_password = PasswordField('Repeat Password')
 
