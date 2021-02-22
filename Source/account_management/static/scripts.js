c527d5445c89d4657697dda08953a532a6fe569c
pb97 <pavel.belitskiy@gmail.com>
2021-02-22 20:22:14 +0000

    Creates dedicated JavaScript file

---
+++ Source/account_management/static/scripts.js
@@ -0,0 +1,8 @@
+function openLogin() {
+  document.getElementById("account_popup").style.display = "block";
+}
+
+function closeLogin(){
+  document.getElementById("account_popup").style.display = "none";
+}
+
