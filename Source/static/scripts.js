5b440e8f252070373cda82addc991a5bf02c28f7
niamh88 <78096390+niamh88@users.noreply.github.com>
2021-03-07 12:20:57 +0000

    updated to include dark mode

--- Source/static/scripts.js
+++ Source/static/scripts.js
@@ -6,6 +6,28 @@ function closeLogin(){
   document.getElementById("account_popup").style.display = "none";
 }
 
+var currTheme = localStorage.getItem("theme");
+if(currTheme=="dark"){
+	document.documentElement.setAttribute("theme", "dark");
+	document.getElementById("theme").checked=true;
+}else{
+	document.documentElement.setAttribute("theme", "");
+	document.getElementById("theme").checked=false;
+}
+
+
+function changeTheme(){
+	if(document.getElementById("theme").checked==true){
+		document.documentElement.setAttribute("theme", "dark");
+		document.documentElement.classList.add("change");
+		localStorage.setItem("theme", "dark");
+	}else{
+		document.documentElement.setAttribute("theme", "");
+		document.documentElement.classList.add("change");
+		localStorage.setItem("theme", "");
+	}
+}
+
 setTimeout(function() {
     $('#message_box_container').delay(2000).fadeOut(1000);
 }, 3000);
